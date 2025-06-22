fd65a73ebaeda2ce94d9923bbd0077a55b44d6f1
ivanskyi <olehfromukraine@gmail.com>
2025-06-22 20:36:27 +0300

    added backend and frontend projects

---
+++ backend/src/test/java/today/trainme/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package today.trainme.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
