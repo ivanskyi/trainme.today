eac09f82a37f0f58ce72368153ae14515964bc42
ivanskyi <olehfromukraine@gmail.com>
2025-06-30 21:31:25 +0300

    Added changes

--- frontend/src/app/component/public-dashboard/public-dashboard.component.ts
+++ frontend/src/app/component/public-dashboard/public-dashboard.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+interface TopicSummary {
+  id: string;
+  name: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-public-dashboard',
   templateUrl: './public-dashboard.component.html',
   styleUrls: ['./public-dashboard.component.scss']
 })
-export class PublicDashboardComponent {
+export class PublicDashboardComponent implements OnInit {
+  topics: TopicSummary[] = [];
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit() {
+    this.loadTopicsList();
+  }
+
+  loadTopicsList() {
+    this.http.get<TopicSummary[]>('assets/topics/topics-list.json').subscribe(data => {
+      this.topics = data;
+    });
+  }
+
+  openTopic(topicId: string) {
+    this.router.navigate(['/topic'], { queryParams: { id: topicId } });
+  }
 }
